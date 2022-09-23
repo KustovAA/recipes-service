a8f8b6b63e4d29b477a8ba91db3b5a656ae06363
Андрей Кустов <andrey_kustov@MacBook-Pro-Andrej.local>
2022-09-23 19:24:07 +1000

    add verbose names to model fields

--- recipes/models.py
+++ recipes/models.py
@@ -2,45 +2,45 @@ from django.db import models
 
 
 class Unit(models.Model):
-    name = models.CharField(max_length=200)
+    name = models.CharField(max_length=200, verbose_name='Название')
 
 
 class Ingredient(models.Model):
-    name = models.CharField(max_length=200)
+    name = models.CharField(max_length=200, verbose_name='Название')
 
 
 class Cuisine(models.Model):
-    name = models.CharField(max_length=200)
+    name = models.CharField(max_length=200, verbose_name='Название')
 
 
 class Category(models.Model):
-    name = models.CharField(max_length=200)
+    name = models.CharField(max_length=200, verbose_name='Название')
 
 
 class Recipe(models.Model):
-    name = models.CharField(max_length=200)
-    category = models.ForeignKey(Category, on_delete=models.SET_NULL, null=True)
-    cuisine = models.ForeignKey(Cuisine, on_delete=models.SET_NULL, null=True)
-    vegetarian = models.BooleanField(default=False)
-    spicy = models.BooleanField(default=False)
-    description = models.TextField()
-    img = models.CharField(max_length=500)
+    name = models.CharField(max_length=200, verbose_name='Название')
+    category = models.ForeignKey(Category, on_delete=models.SET_NULL, null=True, verbose_name='Категория')
+    cuisine = models.ForeignKey(Cuisine, on_delete=models.SET_NULL, null=True, verbose_name='Кухня')
+    vegetarian = models.BooleanField(default=False, verbose_name='Вегетарианское?')
+    spicy = models.BooleanField(default=False, verbose_name='Острое?')
+    description = models.TextField(verbose_name='Рецепт')
+    img = models.CharField(max_length=500, verbose_name='Картинка')
 
 
 class IngredientAndRecipe(models.Model):
-    ingredient = models.ForeignKey(Ingredient, on_delete=models.CASCADE)
-    recipe = models.ForeignKey(Recipe, on_delete=models.CASCADE)
-    amount = models.IntegerField(default=0)
-    unit = models.ForeignKey(Unit, on_delete=models.SET_NULL, null=True)
+    ingredient = models.ForeignKey(Ingredient, on_delete=models.CASCADE, verbose_name='Ингридиент')
+    recipe = models.ForeignKey(Recipe, on_delete=models.CASCADE, verbose_name='Рецепт блюда')
+    amount = models.IntegerField(default=0, verbose_name='Количество')
+    unit = models.ForeignKey(Unit, on_delete=models.SET_NULL, null=True, verbose_name='Единица измерения')
 
 
 class User(models.Model):
-    first_name = models.CharField(max_length=200, null=True)
-    last_name = models.CharField(max_length=200, null=True)
-    phone = models.CharField(max_length=200, null=True)
-    email = models.EmailField(null=True)
-    telegram_id = models.CharField(max_length=200)
-    password = models.CharField(max_length=200, null=True)
-    likes = models.ManyToManyField(Recipe, related_name='likes')
-    dislikes = models.ManyToManyField(Recipe, related_name='dislikes')
+    first_name = models.CharField(max_length=200, null=True, verbose_name='Имя')
+    last_name = models.CharField(max_length=200, null=True, verbose_name='Фамилия')
+    phone = models.CharField(max_length=200, null=True, verbose_name='Телефон')
+    email = models.EmailField(null=True, verbose_name='Почта')
+    telegram_id = models.CharField(max_length=200, verbose_name='телеграм id')
+    password = models.CharField(max_length=200, null=True, verbose_name='Пароль')
+    likes = models.ManyToManyField(Recipe, related_name='likes', verbose_name='Понравившиеся')
+    dislikes = models.ManyToManyField(Recipe, related_name='dislikes', verbose_name='Не понравившиеся')
 
