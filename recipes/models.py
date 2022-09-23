7d26aeb98a2d147639390617d0a2e538d3b3fe76
Андрей Кустов <andrey_kustov@MacBook-Pro-Andrej.local>
2022-09-23 19:36:49 +1000

    add __str__ to models

--- recipes/models.py
+++ recipes/models.py
@@ -4,18 +4,27 @@ from django.db import models
 class Unit(models.Model):
     name = models.CharField(max_length=200, verbose_name='Название')
 
+    def __str__(self):
+        return self.name
+
 
 class Ingredient(models.Model):
     name = models.CharField(max_length=200, verbose_name='Название')
 
+    def __str__(self):
+        return self.name
 
 class Cuisine(models.Model):
     name = models.CharField(max_length=200, verbose_name='Название')
 
+    def __str__(self):
+        return self.name
 
 class Category(models.Model):
     name = models.CharField(max_length=200, verbose_name='Название')
 
+    def __str__(self):
+        return self.name
 
 class Recipe(models.Model):
     name = models.CharField(max_length=200, verbose_name='Название')
@@ -26,12 +35,17 @@ class Recipe(models.Model):
     description = models.TextField(verbose_name='Рецепт')
     img = models.CharField(max_length=500, verbose_name='Картинка')
 
+    def __str__(self):
+        return self.name
 
 class IngredientAndRecipe(models.Model):
     ingredient = models.ForeignKey(Ingredient, on_delete=models.CASCADE, verbose_name='Ингридиент')
     recipe = models.ForeignKey(Recipe, on_delete=models.CASCADE, verbose_name='Рецепт блюда')
     amount = models.IntegerField(default=0, verbose_name='Количество')
     unit = models.ForeignKey(Unit, on_delete=models.SET_NULL, null=True, verbose_name='Единица измерения')
+
+    def __str__(self):
+        return f'{self.ingredient.name} для {self.recipe.name}'
 
 
 class User(models.Model):
@@ -44,3 +58,5 @@ class User(models.Model):
     likes = models.ManyToManyField(Recipe, related_name='likes', verbose_name='Понравившиеся')
     dislikes = models.ManyToManyField(Recipe, related_name='dislikes', verbose_name='Не понравившиеся')
 
+    def __str__(self):
+        return f'{self.first_name} {self.last_name}'
