852c31edcf754833ab3770d7460b844d5a54bdf0
Андрей Кустов <andrey_kustov@MacBook-Pro-Andrej.local>
2022-09-27 14:51:51 +1000

    add UserAndRecipe to admin

--- recipes/admin.py
+++ recipes/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 
-from .models import Unit, Ingredient, IngredientAndRecipe, Cuisine, Recipe, Category, User
+from .models import Unit, Ingredient, IngredientAndRecipe, Cuisine, Recipe, Category, User, UserAndRecipe
 
 admin.site.register(Unit)
 admin.site.register(Ingredient)
@@ -9,3 +9,4 @@ admin.site.register(Cuisine)
 admin.site.register(Recipe)
 admin.site.register(Category)
 admin.site.register(User)
+admin.site.register(UserAndRecipe)
